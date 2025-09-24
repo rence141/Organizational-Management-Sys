2b9ae526c56a69bd7a14facbe49fa47fb177e45a
rence141 <Lorenzezz0987@gmail.com>
2025-09-24 20:25:50 +0800

    new

--- app.js
+++ app.js
@@ -18,3 +18,13 @@ app.use('/api', userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+
+// 404 middleware (for routes not found)
+app.use((req, res, next) => res.status(404).json({ message: 'Route not found' }));
+
+// Centralized error handler (for unexpected errors)
+app.use((err, req, res, next) => {
+  console.error(err);                             // Log the actual error for debugging
+  return res.status(500).json({ message: 'Internal Server Error' });
+});
